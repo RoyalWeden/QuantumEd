1159e4f587f43fd9b1066a923807fc0666ff4749
RoyalWeden <roy.weden@yahoo.com>
2022-01-24 21:41:37 -0600

    workable code editor

---
+++ components/code-item.js
@@ -0,0 +1,149 @@
+import {
+    Box,
+    Text,
+    Button,
+    Container,
+    Textarea
+} from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+import "ace-builds"
+import React, { Component, useState, useEffect } from 'react'
+import AceEditor from "react-ace"
+
+import 'ace-builds/src-noconflict/mode-python'
+import 'ace-builds/src-noconflict/mode-markdown'
+import 'ace-builds/src-noconflict/theme-chrome'
+import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/theme-terminal'
+
+export default class CodeItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            input: props.input || ``,
+            output: props.output || ``,
+            userInput: props.userInput || ``,
+            stdout: ``,
+            stderr: ``,
+            data: [],
+            dataPromise: [],
+            run: false,
+            returned: true,
+        };
+    }
+
+    onRun = (e) => {
+        e.preventDefault();
+        this.setState({ run: true });
+    }
+
+    onInputChange = (e) => {
+        this.setState({ input: e });
+    }
+
+    onUserInputChange = (e) => {
+        this.setState({ userInput: e });
+    }
+
+    componentDidUpdate() {
+        if(this.state.run) {
+            this.setState({ returned: false });
+            fetch("https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=true&wait=true", {
+                "method": "POST",
+                "headers": {
+                    "content-type": "application/json",
+                    "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+                    "x-rapidapi-key": "5d6d8bed5amsh83fd1497b24dc4bp18af10jsn1c99a93dd603",
+                },
+                "body": JSON.stringify({
+                    "language_id": 71,
+                    "source_code": Buffer.from(this.state.input).toString('base64'),
+                    "stdin": Buffer.from(this.state.userInput).toString('base64')
+                }),
+            }).then(
+                res => {
+                    this.setState({...this.state, dataPromise: res.json()});
+                    this.state.dataPromise.then(
+                        val => {
+                            this.setState({ data: val });
+                            this.setState({ stdout: Buffer.from(String(this.state.data.stdout), 'base64') });
+                            this.setState({ stderr: Buffer.from(String(this.state.data.stderr), 'base64') });
+                            if(this.state.data.stdout) {
+                                if(this.state.data.stderr) {
+                                    this.setState({ output: this.state.stdout + this.state.stderr });
+                                } else {
+                                    this.setState({ output: this.state.stdout });
+                                }
+                            } else {
+                                this.setState({ output: this.state.stderr });
+                            }
+                        }
+                    )
+            });
+            this.setState({ run: false });
+        }
+    }
+
+    render() { 
+        return (
+            <Box w="100%">
+                <AceEditor
+                    required
+                    id="input"
+                    mode="python"
+                    theme="monokai"
+                    onChange={this.onInputChange}
+                    name="UNIQUE_ID_OF_DIV"
+                    value={this.state.input}
+                    className=" source"
+                    height="200px"
+                    editorProps={{
+                        $blockScrolling: true
+                    }}
+                />
+                <Button
+                    type="submit"
+                    size="lg"
+                    height="45px"
+                    fontSize={30}
+                    onClick={this.onRun}
+                >
+                    Run
+                </Button>
+                <Text>Result:</Text>
+                <Textarea
+                    id="output"
+                    fontSize={20}
+                    height="100px"
+                    value={this.state.output}
+                    isReadOnly
+                />
+                <Text>User Input:</Text>
+                <AceEditor
+                    required
+                    id="userInput"
+                    mode="markdown"
+                    theme="terminal"
+                    onChange={this.onUserInputChange}
+                    name="UNIQUE_ID_OF_DIV"
+                    value={this.state.userInput}
+                    className=" source"
+                    height="75px"
+                    editorProps={{
+                        $blockScrolling: true
+                    }}
+                />
+            </Box>
+        )
+    }
+}
+    
+export const CodeItemStyle = () => (
+    <Global styles={`
+            .code-item {
+                border-radius: 12px;
+            }
+        `}
+    />
+)
---
+++ pages/learn.js
@@ -0,0 +1,35 @@
+import {
+    Container,
+    // Box,
+    // Divider,
+    Heading,
+    // SimpleGrid,
+    // Button
+} from '@chakra-ui/react'
+import P from '../components/paragraph'
+import Section from '../components/section'
+import Layout from '../components/layouts/article'
+import dynamic from 'next/dynamic'
+import React from 'react'
+
+const DynamicCodeItem = dynamic(
+    () => import('../components/code-item'),
+    { ssr: false }
+)
+
+const Learn = () => {
+    return (
+        <Layout>
+            <Container>
+                <Section>
+                    <P>
+                        Feel free to test some Python code while this page is being created.
+                    </P>
+                    <DynamicCodeItem input="print('Hello World!')" />
+                </Section>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Learn
